33e3bb6c18548be73d569e39adf1f86c836d5a2a
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-10-09 22:17:48 -0500

    ProductList

---
+++ components/products/ProductList.tsx
@@ -0,0 +1,23 @@
+import { Grid } from '@mui/material';
+import { IProduct } from '../../interfaces';
+import { ProductCard } from './ProductCard';
+
+
+interface Props {
+  products: IProduct[];
+}
+
+export const ProductList: React.FC<Props> = ({products}) => {
+  return (
+    <Grid container spacing={4}>
+      {
+        products.map(product => (
+          <ProductCard
+            product={product}
+            key={product.slug} // todo: Reemplazar por product._id cuando se conecte a database.
+          />
+        ))
+      }
+    </Grid>
+  );
+};
