e3ad4dd60f4d69369c1353f62b42f194bda9058e
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-10-10 10:40:57 -0500

    react-slideshow-image: Se instaló la version 3.7.0 para aprender como lucen los archivos de definicion de Typescript realizados manualmente

---
+++ components/products/ProductSlideshow.tsx
@@ -0,0 +1,14 @@
+import { Slide } from 'react-slideshow-image';
+
+interface Props {
+  images: string[];
+}
+
+export const ProductSlideshow: React.FC<Props> = ({images}) => {
+
+  return (
+    <Slide>
+      
+    </Slide>
+  );
+};
