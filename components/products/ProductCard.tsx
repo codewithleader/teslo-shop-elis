48386c4bd10cf94819c3b0fc537f6fac2ba64786
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-10-10 10:18:43 -0500

    ProductPage

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,4 +1,6 @@
-import { Grid, Card, CardActionArea, CardMedia } from '@mui/material';
+import { useMemo, useState } from 'react';
+import NextLink from 'next/link';
+import { Grid, Card, CardActionArea, CardMedia, Box, Typography, Link } from '@mui/material';
 import { IProduct } from '../../interfaces';
 
 interface Props {
@@ -6,13 +8,34 @@ interface Props {
 }
 
 export const ProductCard: React.FC<Props> = ({ product }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const productImage = useMemo(() => {
+    return isHovered ? `products/${product.images[1]}` : `products/${product.images[0]}`;
+  }, [isHovered, product.images]);
+
   return (
-    <Grid item xs={6} sm={4}>
+    <Grid
+      item
+      xs={6}
+      sm={4}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <Card>
-        <CardActionArea>
-          <CardMedia component='img' image={`products/${product.images[0]}`} alt={product.title} />
-        </CardActionArea>
+        <NextLink href={'/product/slug'} passHref prefetch={false}>
+          <Link>
+            <CardActionArea>
+              <CardMedia component='img' image={productImage} alt={product.title} className='fadeIn' />
+            </CardActionArea>
+          </Link>
+        </NextLink>
       </Card>
+
+      <Box sx={{ mt: 1 }} className='fadeIn'>
+        <Typography fontWeight={700}>{product.title}</Typography>
+        <Typography fontWeight={500}>{`$${product.price}`}</Typography>
+      </Box>
     </Grid>
   );
 };
---
+++ pages/product/slug.tsx
@@ -0,0 +1,50 @@
+import { Button, Chip, Grid, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { ShopLayout } from '../../components/layouts';
+import { initialData } from '../../database/products';
+
+const product = initialData.products[0];
+
+export const ProductPage = () => {
+  return (
+    <ShopLayout title={product.title} pageDescription={product.description}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={7}>
+          {/* TODO: SlideShow */}
+        </Grid>
+
+        <Grid item xs={12} sm={5}>
+          <Box display='flex' flexDirection='column'>
+            {/* Title */}
+            <Typography variant='h1' component='h1'>
+              {product.title}
+            </Typography>
+            <Typography variant='subtitle1' component='h2'>{`$${product.price}`}</Typography>
+
+            {/* Quantity */}
+            <Box sx={{ my: 2 }}>
+              <Typography variant='subtitle2'>Quantity</Typography>
+
+              {/* TODO: Counter */}
+            </Box>
+
+            {/* Add to Cart */}
+            <Button color='secondary' className='circular-btn'>
+              Add to Cart
+            </Button>
+
+            {/* <Chip label={'Unavailable'} color='error' variant='outlined' /> */}
+
+            {/* Description */}
+            <Box sx={{mt:3}}>
+              <Typography variant='subtitle2'>Description</Typography>
+              <Typography variant='body2'>{product.description}</Typography>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ShopLayout>
+  );
+};
+
+export default ProductPage;
