88399d9a79fe7569233d8e2acbde6b8ae4922aa5
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-10-21 07:37:13 -0500

    [slug].ts: Lo que está entre brackets el nombre de la propiedad enviada como query params

---
+++ pages/api/products/README.md
@@ -0,0 +1,11 @@
+# Los archivos tipo [something].ts
+
+- Se envía en la Request como `query params` siendo lo que se encuentre entre brackets el nombre de la propiedad
+
+Ejemplo de la request.query en el file [slug].ts:
+
+```bash
+"req.query": {
+    "slug": "mens_chill_crew_neck_sweatshirt"
+  }
+```
---
+++ pages/api/products/[slug].ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequestQuery } from 'next/dist/server/api-utils';
+import { db } from '../../../database';
+import { IProduct } from '../../../interfaces';
+import { ProductModel } from '../../../models';
+
+type Data =
+  | {
+      message: string;
+      query: NextApiRequestQuery; // para ver cual fue la consulta (Leer Readme.md para lo del [slug].ts)
+    }
+  | IProduct;
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  switch (req.method) {
+    case 'GET':
+      return getProductBySlug(req, res);
+
+    default:
+      return res.status(400).json({ message: 'Bad request | Elis', query: req.query });
+  }
+}
+
+const getProductBySlug = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  await db.connect();
+
+  const { slug } = req.query;
+  const product = await ProductModel.findOne({ slug }).lean();
+
+  await db.disconnect();
+
+  if (!product) {
+    return res.status(404).json({ message: 'Product not fount', query: req.query });
+  }
+
+  return res.status(200).json(product);
+};
