9f382f48537f0cb69b3cec4f0467911ce675ceb5
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-10-26 16:22:25 -0500

    Moví la carpeta hooks/ a la raiz donde debí haberla creado desde el principio jeje

--- hooks/useProducts.ts
+++ hooks/useProducts.ts
@@ -1,5 +1,5 @@
 import useSWR, { SWRConfiguration } from 'swr';
-import { IProduct } from '../../interfaces';
+import { IProduct } from '../interfaces';
 
 // const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
 
