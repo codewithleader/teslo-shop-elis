2c3ded7c18fa81ce1f5b06757cea3b59cf030d2f
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-10-27 09:17:12 -0500

    Mostrar texto solo cuando la imagen de la Card esté cargada

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -9,6 +9,7 @@ interface Props {
 
 export const ProductCard: React.FC<Props> = ({ product }) => {
   const [isHovered, setIsHovered] = useState(false);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
 
   const productImage = useMemo(() => {
     return isHovered ? `products/${product.images[1]}` : `products/${product.images[0]}`;
@@ -26,13 +27,19 @@ export const ProductCard: React.FC<Props> = ({ product }) => {
         <NextLink href={'/product/slug'} passHref prefetch={false}>
           <Link>
             <CardActionArea>
-              <CardMedia component='img' image={productImage} alt={product.title} className='fadeIn' />
+              <CardMedia
+                alt={product.title}
+                className='fadeIn'
+                component='img'
+                image={productImage}
+                onLoad={() => setIsImageLoaded(true)}
+              />
             </CardActionArea>
           </Link>
         </NextLink>
       </Card>
 
-      <Box sx={{ mt: 1 }} className='fadeIn'>
+      <Box sx={{ mt: 1, display: isImageLoaded ? 'block' : 'none' }} className='fadeIn'>
         <Typography fontWeight={700}>{product.title}</Typography>
         <Typography fontWeight={500}>{`$${product.price}`}</Typography>
       </Box>
