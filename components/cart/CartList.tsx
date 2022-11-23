aeb27dc16bd905ec349a4c953dac493e4bde9ad3
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-11-23 11:34:36 -0500

    get products from State with useContext

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,3 +1,6 @@
+import { useContext } from 'react';
+import { CartContext } from '../../context';
+
 import NextLink from 'next/link';
 
 // import { Box, Button, CardActionArea, CardMedia, Grid, Link, Typography } from '@mui/material'; // No usar asi porque es lento en dev.
@@ -12,16 +15,19 @@ import Typography from '@mui/material/Typography';
 import { initialData } from '../../database/products';
 import { ItemCounter } from '../ui';
 
-const productsInCart = [initialData.products[0], initialData.products[1], initialData.products[2]];
+// const productsInCart = [initialData.products[0], initialData.products[1], initialData.products[2]];
 
 interface Props {
   editable?: boolean;
 }
 
 export const CartList: React.FC<Props> = ({ editable = false }) => {
+  // Get products from State
+  const { cart } = useContext(CartContext);
+
   return (
     <>
-      {productsInCart.map(product => (
+      {cart.map(product => (
         <Grid container spacing={2} key={product.slug} sx={{ mb: 1 }}>
           <Grid item xs={3}>
             {/* TODO: Llevar a la pagina del producto */}
@@ -30,7 +36,7 @@ export const CartList: React.FC<Props> = ({ editable = false }) => {
                 <CardActionArea>
                   <CardMedia
                     // props
-                    image={`/products/${product.images[0]}`}
+                    image={`/products/${product.image}`}
                     component='img'
                     sx={{ borderRadius: '5px' }}
                   />
@@ -45,7 +51,16 @@ export const CartList: React.FC<Props> = ({ editable = false }) => {
                 Size: <strong>M</strong>
               </Typography>
 
-              {editable ? <ItemCounter /> : <Typography variant='h5'>3 items</Typography>}
+              {editable
+                ? (
+                  <ItemCounter
+                  currentValue={product.quantity}
+                  maxValue={10}
+                  updatedQuantity={() => {}} />
+                )
+                : (
+                <Typography variant='h5'>{product.quantity} {product.quantity > 1 ? 'products' : 'product'}</Typography>
+              )}
             </Box>
           </Grid>
           <Grid item xs={2} display='flex' flexDirection='column' alignItems='center'>
