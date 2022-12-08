3305202700537d039cbbd546c39186bdb703ce19
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-12-07 20:53:52 -0500

    React-Hook-Form

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,5 +1,7 @@
 import NextLink from 'next/link';
 
+import { useForm } from 'react-hook-form';
+
 // import { Box, Button, Divider, Grid, Link, TextField, Typography } from '@mui/material'; // No usar asi porque es mas lento en dev.
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -9,41 +11,88 @@ import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 // import Divider from '@mui/material/Divider';
 
-
 import { AuthLayout } from '../../components/layouts';
+import { validations } from '../../utils';
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 export const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  console.log('errores:', { errors });
+
+  const onLoginUser = (data: FormData) => {
+    console.log('data:', { data });
+  };
+
   return (
     <AuthLayout title={'Sign In'}>
-      <Box sx={{ width: 350, padding: '10px 20px' }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant='h1' component='h1'>
-              Welcome
-            </Typography>
-          </Grid>
+      <form onSubmit={handleSubmit(onLoginUser)} noValidate>
+        <Box sx={{ width: 350, padding: '10px 20px' }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant='h1' component='h1'>
+                Welcome
+              </Typography>
+            </Grid>
 
-          <Grid item xs={12}>
-            <TextField label='Email' variant='filled' fullWidth />
-          </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type='email'
+                label='Email'
+                variant='filled'
+                fullWidth
+                {...register('email', {
+                  required: 'This field is required',
+                  validate: validations.isEmail
+                })}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
+            </Grid>
 
-          <Grid item xs={12}>
-            <TextField label='Password' type='password' variant='filled' fullWidth />
-          </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label='Password'
+                type='password'
+                variant='filled'
+                fullWidth
+                {...register('password', {
+                  required: 'This field is required',
+                  minLength: { value: 6, message: 'Minimum 6 characters' },
+                })}
+                error={!!errors.password}
+                helperText={errors.password?.message}
+              />
+            </Grid>
 
-          <Grid item xs={12}>
-            <Button color='secondary' className='circular-btn' size='large' fullWidth>
-              Sign In
-            </Button>
-          </Grid>
+            <Grid item xs={12}>
+              <Button
+                type='submit'
+                color='secondary'
+                className='circular-btn'
+                size='large'
+                fullWidth
+              >
+                Sign In
+              </Button>
+            </Grid>
 
-          <Grid item xs={12} display='flex' justifyContent='end'>
-            <NextLink href='/auth/register' passHref>
-              <Link underline='always'>Create Account</Link>
-            </NextLink>
+            <Grid item xs={12} display='flex' justifyContent='end'>
+              <NextLink href='/auth/register' passHref>
+                <Link underline='always'>Create Account</Link>
+              </NextLink>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </form>
     </AuthLayout>
   );
 };
--- utils/jwt.ts
+++ utils/jwt.ts
@@ -14,6 +14,26 @@ export const signToken = (_id: string, email: string) => {
     process.env.JWT_SECRET_SEED,
 
     // Options
-    { expiresIn: '30d',}
+    { expiresIn: '30d' }
   );
+};
+
+export const isValidToken = (token: string): Promise<string> => {
+  if (!process.env.JWT_SECRET_SEED) {
+    throw new Error('No hay semilla de JWT - Revisar Variables de Entorno');
+  }
+
+  return new Promise((resolve, reject) => {
+    try {
+      jwt.verify(token, process.env.JWT_SECRET_SEED || '', (error, payload) => {
+        if (error) return reject('JWT no es válido');
+
+        const { _id } = payload as { _id: string };
+
+        resolve(_id);
+      });
+    } catch (error) {
+      reject('JWT no es válido');
+    }
+  });
 };
