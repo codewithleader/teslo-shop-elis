ab2ea74d33cc3f33a8c06938b5766982edf71b5a
Elis Antonio Perez <elisperezmusic@gmail.com>
2023-01-05 11:49:08 -0500

    personalizado el typado en next-auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -22,8 +22,8 @@ export default NextAuth({
       },
     }),
     GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID || '',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
     }),
     // ...add more providers here
   ],
