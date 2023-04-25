248f1e81fbd1fdc39d835cf76618e2844fe3c487
ElisPerez <elisperezmusic@gmail.com>
2023-04-25 11:01:05 -0500

    middleware.ts: Agregada la ruta /admin para verificar roles validos

--- middleware.ts
+++ middleware.ts
@@ -13,10 +13,27 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(url);
   }
 
+  if (req.nextUrl.pathname.startsWith('/admin')) {
+    const validRoles = ['admin', 'super-user', 'CEO'];
+
+    if (!validRoles.includes(session.user.role)) {
+      // Opcion de Fernando:
+      // const url = req.nextUrl.clone();
+      // url.pathname = '/';
+      // return NextResponse.redirect(url);
+
+      // Opcion de documentacion:
+      return NextResponse.rewrite(new URL('/404', req.url)); // Esta te permite mostrar otra pagina pero con el path solicitado (muestra la pagina 404 pero con el path de /admin). Quiero aquí que muestre la página 403 Forbiden pero no la he creado.
+
+      // Otra opcion para redirigir al home:
+      // return NextResponse.redirect(new URL('/', req.url)); // Esta redirige y reemplaza el path
+    }
+  }
+
   // return NextResponse.redirect(`/auth/login?page=${requestedPage}`);
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/checkout/address', '/checkout/summary'],
+  matcher: ['/checkout/address', '/checkout/summary', '/admin'],
 };
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,7 +1,12 @@
+import { useEffect, useState } from 'react';
 import { NextPage } from 'next';
+// el react query para nextjs
+import useSWR from 'swr';
+// layouts
 import { AdminLayout } from 'components/layouts';
 // mui
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 // material/icons
 import GroupOutlined from '@mui/icons-material/GroupOutlined';
 import CategoryOutlined from '@mui/icons-material/CategoryOutlined';
@@ -14,8 +19,44 @@ import CancelPresentationOutlined from '@mui/icons-material/CancelPresentationOu
 import ProductionQuantityLimitsOutlined from '@mui/icons-material/ProductionQuantityLimitsOutlined';
 // components
 import { SummaryTile } from 'components/admin';
+// types
+import { DashboardSummaryResponse } from 'interfaces';
 
 export const DashboardPage: NextPage = () => {
+  const { data, error } = useSWR<DashboardSummaryResponse>('/api/admin/dashboard', {
+    refreshInterval: 30 * 1000, // 30 seg
+  });
+
+  const [refreshIn, setRefreshIn] = useState(30);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log('tick')
+      setRefreshIn(refreshIn => refreshIn > 0 ? refreshIn -1 : 30);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (!error && !data) {
+    return <></>;
+  }
+
+  if (error) {
+    console.log(error);
+    return <Typography>Error al cargar la información</Typography>;
+  }
+
+  const {
+    paidOrders,
+    lowInventory,
+    notPaidOrders,
+    numberOfOrders,
+    numberOfClients,
+    numberOfProducts,
+    productsWithNoInventory,
+  } = data!;
+
   return (
     <AdminLayout
       //
@@ -25,49 +66,49 @@ export const DashboardPage: NextPage = () => {
     >
       <Grid container spacing={2}>
         <SummaryTile
-          title={'numberOfOrders'}
+          title={numberOfOrders}
           subTitle='Ordenes totales'
           icon={<CreditCardOutlined color='secondary' sx={{ fontSize: 40 }} />}
         />
 
         <SummaryTile
-          title={'paidOrders'}
+          title={paidOrders}
           subTitle='Ordenes pagadas'
           icon={<AttachMoneyOutlined color='success' sx={{ fontSize: 40 }} />}
         />
 
         <SummaryTile
-          title={'notPaidOrders'}
+          title={notPaidOrders}
           subTitle='Ordenes pendientes'
           icon={<CreditCardOffOutlined color='error' sx={{ fontSize: 40 }} />}
         />
 
         <SummaryTile
-          title={'numberOfClients'}
+          title={numberOfClients}
           subTitle='Clientes'
           icon={<GroupOutlined color='primary' sx={{ fontSize: 40 }} />}
         />
 
         <SummaryTile
-          title={'numberOfProducts'}
+          title={numberOfProducts}
           subTitle='Productos'
           icon={<CategoryOutlined color='warning' sx={{ fontSize: 40 }} />}
         />
 
         <SummaryTile
-          title={'productsWithNoInventory'}
+          title={productsWithNoInventory}
           subTitle='Sin existencias'
           icon={<CancelPresentationOutlined color='error' sx={{ fontSize: 40 }} />}
         />
 
         <SummaryTile
-          title={'lowInventory'}
+          title={lowInventory}
           subTitle='Bajo inventario'
           icon={<ProductionQuantityLimitsOutlined color='warning' sx={{ fontSize: 40 }} />}
         />
 
         <SummaryTile
-          title={'refreshIn'}
+          title={refreshIn}
           subTitle='Actualización en:'
           icon={<AccessTimeOutlined color='secondary' sx={{ fontSize: 40 }} />}
         />
